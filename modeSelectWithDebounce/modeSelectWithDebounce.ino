30eb4af75fe8024ace89fb7b61fe5965a3866ec4
gcronin <gabriel.cronin@gmail.com>
2022-04-10 09:22:14 -0700

    Update modeSelectWithDebounce.ino

--- modeSelectWithDebounce/modeSelectWithDebounce.ino
+++ modeSelectWithDebounce/modeSelectWithDebounce.ino
@@ -1,21 +1,26 @@
 int numModes = 5;
-volatile int mode = 0;
+volatile int modeA = 0;
+volatile int modeB = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), increment, LOW);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), incrementA, LOW);
+  attachInterrupt(digitalPinToInterrupt(3), incrementB, LOW);
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   delay(200);
-    Serial.println(mode);
+  Serial.print(modeA);
+  Serial.print("  ");
+  Serial.println(modeB);
  
 }
 
-void increment()
+void incrementA()
 {
   //Static variable initialized only first time increment called, persists between calls.
   static unsigned long last_interrupt_time = 0;
@@ -23,7 +28,20 @@ void increment()
   // If interrupts come faster than 200ms, assume it's a bounce and ignore
   if (interrupt_time - last_interrupt_time > 200) 
   {
-    mode = (mode+1)%numModes;
+    modeA = (modeA+1)%numModes;
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+void incrementB()
+{
+  //Static variable initialized only first time increment called, persists between calls.
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200) 
+  {
+    modeB = (modeB+1)%numModes;
   }
   last_interrupt_time = interrupt_time;
 }
