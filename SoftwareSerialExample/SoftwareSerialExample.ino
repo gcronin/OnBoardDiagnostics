85e01bd0b3dd65d8cc0a27112082bba0e0db1eb1
gcronin <gabriel.cronin@gmail.com>
2022-04-11 06:53:01 -0700

    Update SoftwareSerialExample.ino

--- SoftwareSerialExample/SoftwareSerialExample.ino
+++ SoftwareSerialExample/SoftwareSerialExample.ino
@@ -11,7 +11,15 @@ char rxIndex=0;
 String data="010C";
 char expResponse[5] = "410C";
 
+int numModes = 2;
+volatile int modeA = 0;
+volatile int modeB = 0;
+
 void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(2), incrementA, LOW);
+  attachInterrupt(digitalPinToInterrupt(3), incrementB, LOW);
   Serial.begin(9600);
   OBD.begin(9600);
   lcd.begin(16, 2);
@@ -107,4 +115,30 @@ int findSync(void) {
     location++;
   }
   return -1;
-}+
+}
+
+void incrementA()
+{
+  //Static variable initialized only first time increment called, persists between calls.
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200) 
+  {
+    modeA = (modeA+1)%numModes;
+  }
+  last_interrupt_time = interrupt_time;
+}
+
+void incrementB()
+{
+  //Static variable initialized only first time increment called, persists between calls.
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = millis();
+  // If interrupts come faster than 200ms, assume it's a bounce and ignore
+  if (interrupt_time - last_interrupt_time > 200) 
+  {
+    modeB = (modeB+1)%numModes;
+  }
+  last_interrupt_time = interrupt_time;
+}
