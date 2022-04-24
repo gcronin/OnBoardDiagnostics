ad2ea23ab6d83f58d73892ff4abb71e4ffc05a88
gcronin <gabriel.cronin@gmail.com>
2022-04-24 15:12:51 -0700

    Working...

--- SoftwareSerialExample/SoftwareSerialExample.ino
+++ SoftwareSerialExample/SoftwareSerialExample.ino
@@ -13,8 +13,9 @@ char rxData[20];
 char rxIndex=0;
 
 // Setup the available OBD codes and associated names
-const int numModes = 8;
-volatile int mode = 6; //starting mode
+const int numModes = 9;
+volatile int mode = 0; //starting mode
+volatile boolean modeChanged = false;
 String PIDcodes[numModes] = {"0104", "0105", "010C", "010D", "010F", "0111", "atrv", "0101"};
 String PIDnames[numModes] = {"Load %     ", "EngTemp oC ", "RPM        ", "Speed km/hr", "AirTemp oC ", "Throttle % ", "Battery V  ", "Numbr Codes"};
 char expResponse[5];
@@ -32,32 +33,28 @@ void setup() {
 
 void loop() { 
   OBD.flush();
-  //emptyRXBuffer();  PROBLEMATIC... SEEMS TO CAUSE FREEZING WITH INTERRUPTS...
+  emptyRXBuffer();
   
   //request data from OBD
   OBD.println(PIDcodes[mode]);
-  delay(100);  
 
   //first response repeats sent data for OBD commands
-  //second response is what we want
+  //second response is what we want.  For AT commands, only one response.
+  if(mode!=6) {  getResponse();  }
+  getResponse();
+
+  //setup expected response code... same as PID with '4' at beginning except AT commands... no change
+  PIDcodes[mode].toCharArray(expResponse, 5);
+  if(mode!=6) { expResponse[0] = '4'; }
+
+  // find expected response code in received response 
   int syncLocation;
-  if(mode!=6) {
-    getResponse();
-    getResponse();
-
-    //setup expected response code... same as PID with '4' at beginning
-    PIDcodes[mode].toCharArray(expResponse, 5);
-    expResponse[0] = '4';
+  syncLocation = findSync();
   
-    // find expected response code in received response 
-    syncLocation = findSync();
-  }
-  else syncLocation = 0;
-
   // if we actually found the expected response
   if(syncLocation != -1) {
     // print full response and index of expected response
-    if(useSerial && mode!=-6) {
+    if(useSerial) {
       for(int i=0; i<20; i++)
       { 
         Serial.print(rxData[i]);
@@ -66,8 +63,8 @@ void loop() {
       Serial.print(syncLocation);
     }
 
-    //parse response for out data
-    int data;  float Data;
+    //parse response for our data, filter out spurious results
+    int data;  float voltage;
     switch(mode) {
       case 0: // engine load, percent (max 100)
         data = strtol(&rxData[syncLocation],0,16)*100/255;
@@ -93,12 +90,15 @@ void loop() {
         data = (abs(data) > 100) ? -1 : data;
         break;
       case 6: // battery voltage  (8<V<20)
-        Data = readBattery();
-        data = (Data > 8 && Data < 20) ? 1 : -1;
+        String battResponse = rxData;
+        battResponse = battResponse.substring(syncLocation);
+        voltage = battResponse.toFloat();
+        data = (voltage > 8 && voltage < 20) ? 1 : -1;
         break; 
       case 7: // number codes (max 10)
         String numCodes = rxData;
         numCodes = numCodes.substring(syncLocation, syncLocation+2);
+        Serial.print(numCodes);
         data = strtol(&numCodes[0],0,16) - 128;
         data = (abs(data) > 10) ? -1 : data;
         break;
@@ -107,12 +107,13 @@ void loop() {
         break;        
     }
 
+    //if we received valid data, print it
     if(data != -1) {
       if(useSerial) {
         Serial.print("  ");
         Serial.print(PIDnames[mode]);
         Serial.print("  ");
-        if(mode == 6) Serial.println(Data);
+        if(mode == 6) Serial.println(voltage);
         else Serial.println(data);
       }
 
@@ -122,7 +123,7 @@ void loop() {
         lcd.setCursor(0, 1);
         lcd.print("           ");
         lcd.setCursor(0, 1);
-        if(mode == 6) lcd.print(Data);
+        if(mode == 6) lcd.print(voltage);
         else lcd.print(data);
       }
     }
@@ -131,14 +132,23 @@ void loop() {
   delay(500);
 }
 
-//The getResponse function collects incoming data from the UART into the rxData buffer
-// and only exits when a carriage return character is seen. Once the carriage return
-// string is detected, the rxData buffer is null terminated (so we can treat it as a string)
+// The getResponse function collects incoming data from the UART into the rxData buffer
+// and only exits when a designated end character is seen. Once the end character
+// is detected, the rxData buffer is null terminated (so we can treat it as a string)
 // and the rxData index is reset to 0 so that the next string can be copied.
 void getResponse(){
   char inChar=0;
-  //Keep reading characters until we get a carriage return
-  while(inChar != '\r'){
+  char endCharacter = '\r';  //end of message character ('\r').
+  if(mode == 6) { endCharacter = 'V'; } // in battery voltage mode, look for V
+  //Keep reading characters until we get the end character
+  while(inChar != endCharacter){
+    //Serial.println("In loop");
+    // break out of while loop if interrupt triggered
+    if(modeChanged) {
+      rxIndex = 0;
+      modeChanged = false;
+      break;
+    }
     // prevent buffer overflow
     if(rxIndex > 19) {
       rxIndex = 0;
@@ -146,8 +156,8 @@ void getResponse(){
     }
     //If a character comes in on the serial port, we need to act on it.
     else if(OBD.available() > 0){
-      //Start by checking if we've received the end of message character ('\r').
-      if(OBD.peek() == '\r'){
+      //Start by checking if we've received the end character
+      if(OBD.peek() == endCharacter){
         //Clear the Serial buffer
         inChar=OBD.read();
         //Put the end of string character on our data string
@@ -160,7 +170,7 @@ void getResponse(){
         //Clear the Serial buffer
         inChar=OBD.read();
       }
-      //If we didn't get the end of message character or a space, just add the new character to the string.
+      //If we didn't get the end character or a space, just add the new character to the string.
       else{
         //Get the new character from the Serial port.
         inChar = OBD.read();
@@ -203,6 +213,7 @@ void incrementMode()
   if (interrupt_time - last_interrupt_time > 200) 
   {
     mode = (mode+1)%numModes;
+    modeChanged = true;
   }
   last_interrupt_time = interrupt_time;
 }
@@ -216,30 +227,13 @@ void decrementMode()
   if (interrupt_time - last_interrupt_time > 200) 
   {
     mode = (numModes-1+mode)%numModes;
+    modeChanged = true;
   }
   last_interrupt_time = interrupt_time;
 }
 
 void emptyRXBuffer() {
-  while(OBD.available()>0 ) {
-    OBD.read();
+  while(OBD.available()>0) {
+    OBD.read();   
   }
-}
-
-float readBattery() {
-  boolean receivedString = false;
-  String Data = "";
-  while(!receivedString) {
-    if(OBD.available()) {
-      char character = OBD.read();    
-      if(character == 'V') { 
-          receivedString = true; 
-
-      }
-      else { Data.concat(character); }
-    }
-  }
-  if(useSerial) Serial.print(Data);
-  Data=Data.substring(5);
-  return Data.toFloat();
 }
